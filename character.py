fd939189676318ac8d2f3e600eb669c38ab1d9d8
Andrew Chincea <55414929+andrewc156@users.noreply.github.com>
2024-10-02 18:02:04 -0400

    added jump to character and physic improved a bit

--- character.py
+++ character.py
@@ -8,16 +8,20 @@ class Character(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.V_x = 10
         self.V_y = 0
-        self.position = [positionx, positiony]
+        self.positionx = 0
+        self.positiony = 0
         self.is_jumping = True
         self.window_height = 1080
         self.alive = True
 
-    def get_position(self):
-        return self.position
+    def get_position(self) -> tuple:
+        return (self.positionx, self.positiony)
 
-    def get_velocity(self):
-        return [self.V_x, self.V_y]
+    def get_velocity(self) -> tuple:
+        return (self.V_x, self.V_y)
 
-    def is_alive(self):
+    def is_alive(self) -> bool:
         return self.alive
+
+    def jump(self):
+        self.V_y = 40
--- physics.py
+++ physics.py
@@ -1,7 +1,7 @@
 class Physics:
-    def __init__(self,object):
+    def __init__(self, item):
         self.gravity = 10
 
-
-    def update(self,time,object):
-
+    def update(self, time, item):
+        item.V_y -= self.gravity
+        item.positiony += item.V_y
