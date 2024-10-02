d6ffa96b64a8bd182d128723d8e3a3decae18b05
Andrew Chincea <55414929+andrewc156@users.noreply.github.com>
2024-10-02 17:35:02 -0400

    Added character class

--- character.py
+++ character.py
@@ -3,3 +3,12 @@ import pygame
 class Character(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("Put image here").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.x_velocity = 0
+        self.y_velocity = 0
+        self.is_jumping = True
+        self.gravity = 1
+        self.window_height = 1080
+        self.alive = True
+
