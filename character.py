ae80ac45e0f8b220ca129b722153ec2ad58efab1
Andrew Chincea <55414929+andrewc156@users.noreply.github.com>
2024-10-02 17:42:38 -0400

    added character

--- character.py
+++ character.py
@@ -1,14 +1,22 @@
 import pygame
 
 class Character(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, positionx, positiony):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("Put image here").convert_alpha()
         self.rect = self.image.get_rect()
-        self.x_velocity = 0
-        self.y_velocity = 0
+        self.V_x = 0
+        self.V_x = 0
+        self.position = [positionx, positiony]
         self.is_jumping = True
-        self.gravity = 1
         self.window_height = 1080
         self.alive = True
 
+
+    def get_position(self):
+        return self.position
+
+    def get_velocity(self):
+        return [self.V_x, self.V_y]
+    def is_alive(self):
+        return self.alive
